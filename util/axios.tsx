0dbd90eddfcf453375a0ab23e0895697717f7fa2
acomarcho <acoxstpd@gmail.com>
2024-02-27 22:05:03 +0700

    Feat: Add withCredentials in Axios

--- util/axios.tsx
+++ util/axios.tsx
@@ -5,7 +5,7 @@ import { toast } from "sonner";
 
 export async function axiosPost<T>(url: string, data: unknown): Promise<T> {
 	try {
-		const response = await axios.post<T>(url, data);
+		const response = await axios.post<T>(url, data, { withCredentials: true });
 		return response.data;
 	} catch (error) {
 		if (error instanceof AxiosError) {
