99864056659d4f9a7d3e965d819fe4a599798c03
acomarcho <acoxstpd@gmail.com>
2024-02-27 19:26:13 +0700

    Feat: Adjust styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="max-w-wrapper mx-auto">
         <div className="px-8 py-16">
           <h1 className="text-4xl font-bold text-white">覚えて (Oboete)</h1>
-          <p className="text-base text-blue-200 mt-4 max-w-[30ch]">
+          <p className="text-base text-blue-100 mt-4 max-w-[30ch]">
             Always remember, with the power of simplicity and spaced repetition.
           </p>
           <button className="mt-12 bg-blue-200 text-blue-950 px-4 py-2 font-bold rounded-md transition-all hover:bg-blue-300 hover:cursor-pointer">
