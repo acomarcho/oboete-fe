b0efadb2e46674b1865e49adc003ae54ec65f56a
acomarcho <acoxstpd@gmail.com>
2024-02-28 18:03:35 +0700

    Feat: Initialize framer-motion

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
 		<>
 			<div className="bg-hero-background bg-cover bg-bottom">
 				<div className="max-w-wrapper mx-auto">
-					<div className="px-8 py-16">
+					<div className="px-8 py-20">
 						<h1 className="text-4xl font-bold text-white">覚えて (Oboete)</h1>
 						<p className="text-base text-blue-100 mt-4 max-w-[30ch]">
 							Always remember, with the power of simplicity and spaced
@@ -25,7 +25,7 @@ export default function Home() {
 				<p className="mt-4 uppercase tracking-widest text-sm text-blue-500 text-center">
 					Remember concepts, effortlessly
 				</p>
-				<div className="mt-8 flex flex-col gap-8 lg:flex-row">
+				<div className="mt-12 flex flex-col gap-8 lg:flex-row">
 					<div className="bg-blue-50 p-6 shadow rounded-md space-y-4 text-center">
 						<h2 className="font-bold text-xl text-blue-950 uppercase tracking-wider">
 							Spaced repetition
