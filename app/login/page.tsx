c015c175b57cb33693ca23c8905cb7f8a8767d21
acomarcho <acoxstpd@gmail.com>
2024-02-29 14:47:26 +0700

    Feat: Adjust styling at register and login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/login/form";
 
 export default function RegisterPage() {
 	return (
-		<div className="bg-hero-background bg-cover h-screen bg-bottom grid place-items-center p-8">
+		<div className="bg-hero-background bg-cover min-h-screen bg-bottom grid place-items-center px-8 pb-8 pt-24">
 			<div className="bg-white w-full max-w-[400px] p-8 rounded-md drop-shadow">
 				<h1 className="text-2xl font-bold">Authenticate yourself</h1>
 				<LoginForm />
