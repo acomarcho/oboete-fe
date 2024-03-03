12e18b6252b258228bb3d220699fc829dad0a4c3
acomarcho <acoxstpd@gmail.com>
2024-03-03 10:57:27 +0700

    Feat: Change redirect to /cards after login

--- components/login/form.tsx
+++ components/login/form.tsx
@@ -63,7 +63,7 @@ export default function LoginForm() {
 
 			toast.success("Successfully logged in!");
 
-			router.push("/");
+			router.push("/cards");
 		} catch {
 		} finally {
 			setPageStatus(PageStatus.None);
